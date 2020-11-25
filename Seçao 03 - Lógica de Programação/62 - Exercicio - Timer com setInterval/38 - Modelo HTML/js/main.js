1def0e93b78d72c0077e86cc7ae06b2d515a2fd0
mayconrcampos <maycon.campos@gmail.com>
2020-11-25 19:44:01 -0300

    Exercicio aula 62 - Cronômetro

--- Seçao 03 - Lógica de Programação/62 - Exercicio - Timer com setInterval/38 - Modelo HTML/js/main.js
+++ Seçao 03 - Lógica de Programação/62 - Exercicio - Timer com setInterval/38 - Modelo HTML/js/main.js
@@ -1,39 +1,78 @@
-var tempo = new Date()
-var crono = document.querySelector(".cronometro")
+var mostracrono = document.querySelector(".cronometro")
+var iniciar = document.querySelector(".iniciar")
+var pausar = document.querySelector(".pausar")
+var zerar = document.querySelector(".zerar")
 
-function cronometro(){
-    tempo.setHours(0)
-    tempo.setMinutes(0)
-    tempo.setSeconds(0)
+mostracrono.style.color = "black"
 
-    return tempo.toLocaleTimeString("pt-BR", {
-        
-    })
+var cronometro
+var hora = 0
+var minuto = 0
+var segundo = 0
+
+// Função formata número - Se for menor que 10 ele acrescenta um 0 antes.
+function fmt(num){
+    return num > 9 ? num: `0${num}`
 }
 
-function zerar(){
-    crono.innerHTML = `${cronometro()}`
-}
-
-function iniciar(){
-    const timer = setInterval(function () {
-        
-
-        return tempo.toLocaleTimeString("pt-BR", {
-
-        })
+// Função que gera um cronômetro.
+function timer(){
+    mostracrono.style.color = "black"
+    cronometro = setInterval(function(){
+        if(segundo < 60){
+            segundo++
+        }
+        if(segundo == 60){
+            segundo = 0
+            minuto++
+        }
+        if(minuto == 60){
+            minuto = 0
+            hora++
+        }
+        mostracrono.innerHTML = `${fmt(hora)}:${fmt(minuto)}:${fmt(segundo)}`
     }, 1000)
-   
-   
-    setTimeout(function() {
-        clearInterval(timer)
-    }, 10000)
 }
 
-iniciar()
+// Liga botão iniciar contagem, chama a função timer
+iniciar.addEventListener("click", function(event){
+    if(mostracrono.style.color == "black"){
+        setTimeout(function(){
+            clearInterval(cronometro)
+            hora = 0
+            minuto = 0
+            segundo = 0
+            timer()
+        }, 1)
+        
+    }else{
+        timer()
+    }
+    
+})
 
+// Liga botão pausar - testa cor da letra, se for preta, ele pausa a contagem e passa a letra pra vermelho. Se tiver letra vermelha, ele retoma a contagem e passa a letra para preto.
 
+pausar.addEventListener("click", function(event){
+    if(mostracrono.style.color == "black"){
+        setTimeout(function(){
+            clearInterval(cronometro)
+            mostracrono.style.color = "red"
+        }, 1)
+    }else if(mostracrono.style.color == "red"){
+        timer()
+    }
+})
 
+// Liga botão zerar - Pausa contagem, depois zera cronômetro, depois mostra o cronômetro zerado na tela e muda a cor pra preto.
+zerar.addEventListener("click", function(event){
+    setTimeout(function(){
+        clearInterval(cronometro)
+    }, 1)
 
-
-
+    hora = 0
+    minuto = 0
+    segundo = 0
+    mostracrono.innerHTML = `${fmt(hora)}:${fmt(minuto)}:${fmt(segundo)}`
+    mostracrono.style.color = "black"
+})
