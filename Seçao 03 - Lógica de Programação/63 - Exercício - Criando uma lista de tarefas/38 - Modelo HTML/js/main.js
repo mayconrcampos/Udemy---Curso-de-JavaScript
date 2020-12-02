c2ef1637b7a3d63f425fbd2f645697937515cea7
mayconrcampos <maycon.campos@gmail.com>
2020-12-01 22:05:32 -0300

    Exercício 63 - Parou na metade da aula

--- Seçao 03 - Lógica de Programação/63 - Exercício - Criando uma lista de tarefas/38 - Modelo HTML/js/main.js
+++ Seçao 03 - Lógica de Programação/63 - Exercício - Criando uma lista de tarefas/38 - Modelo HTML/js/main.js
@@ -1,13 +1,49 @@
-const inputTarefa = document.querySelector(".entratarefa").value
+const inputTarefa = document.querySelector(".entratarefa")
 const btnTarefa = document.querySelector(".btntarefa")
 const tarefas = document.querySelector(".tarefas")
 
-const texto = document.querySelector(".texto")
+
+inputTarefa.addEventListener("keypress", function(event){
+    if(event.keyCode === 13){
+        if(!inputTarefa.value) return
+        criaTarefa(inputTarefa.value)
+        limparInput()
+    }
+})
+
+/// Tem que criar a funçao apagar.....
+
+function criaBtnApagar(li){
+    li.innerText += " "
+    const btnApagar = document.createElement("button")
+    btnApagar.innerText = "Apagar"
+    li.appendChild(btnApagar)
+}
+
+function limparInput(){
+    inputTarefa.focus()
+    inputTarefa.value = ""
+}
+
+function criaLista(){
+    const li = document.createElement("li")
+    return li
+}
+
+
+function criaTarefa(txt){
+    const li = criaLista()
+    li.innerHTML = txt
+    tarefas.appendChild(li)  
+    limparInput() 
+    criaBtnApagar(li)
+}
+
 
 btnTarefa.addEventListener("click", function(event){
-    if(inputTarefa){
-        texto.innerHTML = `${inputTarefa}`
+    if(inputTarefa.value){
+        criaTarefa(inputTarefa.value)
     }else{
-        alert("Fudeu")
+        alert("Você deve digitar uma tarefa.")
     }
 })
